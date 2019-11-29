7e9ee4417c32bd82ca35f2b92748a466dc52c79b
dylandalton <dylandalton.za@gmail.com>
2019-11-29 15:31:43 +0000

    Player class variables created, constructor created.

--- src/com/trebletreble/Player.java
+++ src/com/trebletreble/Player.java
@@ -1,4 +1,27 @@
 package com.trebletreble;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    String Name;
+    int score;
+    int balance;
+    ArrayList<Card> hand = new ArrayList<>(0);
+
+    public Player(String Name, int score, int balance, Card[] hand){
+
+    }
+
+    public String hit(){
+        
+        cardDealt = Deck.deal();
+        hand.add(cardDealt);
+
+        return hand;
+    }
+
+
+
+
 }
